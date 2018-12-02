dbb79a8c49e0a3a67b915c5e615067ee906962ae
Nikolay Stoynov <arvigeus@gmail.com>
2018-12-02 15:35:29 +0200

    feat(page): basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { Component } from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import React from "react";
 
-class App extends Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import GlobalStyle from "./style/GlobalStyle";
+
+const App = (): JSX.Element => (
+  <>
+    <GlobalStyle />
+    <main>
+      <h1 id="friendie">Friendie</h1>
+      <p>A social framework for building friendships</p>
+    </main>
+  </>
+);
 
 export default App;
--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
+import background from "./diary.png";
 import { colors, fonts } from "./theme";
 
-// tslint:disable-next-line: typedef variable-name
+// tslint:disable-next-line: typedef
 const GlobalStyle = createGlobalStyle`
   html,
   body {
@@ -15,12 +16,68 @@ const GlobalStyle = createGlobalStyle`
     font-size: 28px;
   }
 
+  * {
+    box-sizing: border-box;
+  }
+
   /* Reset */
   input[type="submit"]:focus,
   input[type="button"]:focus,
   button:focus,
   a:focus {
     outline: 0;
+  }
+
+  #root {
+    position: relative;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    padding: 20px 40px;
+    margin: 0;
+    background-image: url(${background});
+    overflow-x: hidden;
+  }
+
+  main {
+    position: relative;
+    width: 100%;
+    min-height: 300px;
+    box-sizing: border-box;
+    padding: 20px 40px;
+  }
+
+  main,
+  main::before,
+  main::after {
+    border: 1px solid #bbb;
+    background-color: #eee;
+
+    /* Styles to distinguish sheets from one another */
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.25);
+  }
+
+  main::before,
+  main::after {
+    position: absolute;
+    width: 99%;
+    height: 95%;
+    max-height: 600px;
+    content: "";
+  }
+
+  main::before {
+    z-index: -1;
+    top: 0;
+    right: 15px;
+    transform: rotate(-1deg);
+  }
+
+  main::after {
+    z-index: -2;
+    top: 5px;
+    right: -5px;
+    transform: rotate(1deg);
   }
 `;
 
