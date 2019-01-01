685630efac5bc43f29a3568bdeebf422da140e0c
Nikolay Stoynov <arvigeus@gmail.com>
2019-01-02 01:06:23 +0200

    fix(picture): use styled-components/macro
    Also fixes a typo in docs

--- src/components/Picture/Picture.tsx
+++ src/components/Picture/Picture.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { ImgResource } from "the-platform";
 
 import { colors, fonts } from "../../style/theme";
