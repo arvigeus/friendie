2c878fcfa224a5f864d4ba40a3020f78e15ee0af
Nikolay Stoynov <arvigeus@gmail.com>
2019-02-24 21:17:25 +0200

    feat: add Video component

---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import getEmbeddedLink from "./getEmbeddedLink";
+
+interface VideoProps extends React.HTMLProps<HTMLIFrameElement> {
+  /**
+   * Address of video
+   */
+  src: string;
+  /**
+   * Title of iframe
+   */
+  title: string;
+}
+
+const Video = ({ src, title, ...props }: VideoProps): React.ReactChild => (
+  <Filmstrip>
+    <iframe
+      src={getEmbeddedLink(src)}
+      {...props}
+      title={title}
+      frameBorder="0"
+      allowFullScreen
+    />
+  </Filmstrip>
+);
+
+const Filmstrip = styled.div`
+  position: relative;
+  height: 0;
+  padding-top: 25px;
+  padding-bottom: 56.25%;
+  background-color: #000;
+
+  & > iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 99%;
+    height: 98%;
+    box-sizing: border-box;
+    margin: 0.5%;
+  }
+
+  &::before {
+    left: -20px;
+    background: linear-gradient(to bottom, #000 10px, rgba(0, 0, 0, 0) 10px),
+      linear-gradient(to right, #000 10px, rgba(0, 0, 0, 0) 10px);
+  }
+
+  &::after {
+    right: -20px;
+    background: linear-gradient(to bottom, #000 10px, rgba(0, 0, 0, 0) 10px),
+      linear-gradient(to left, #000 10px, rgba(0, 0, 0, 0) 10px);
+  }
+
+  &::before,
+  &::after {
+    position: absolute;
+    top: 0;
+    display: block;
+    width: 20px;
+    height: 100%;
+    background-repeat: repeat-y;
+    background-size: 100% 20px;
+    content: "";
+  }
+`;
+
+export default Video;
---
+++ src/components/Video/getEmbeddedLink.ts
@@ -0,0 +1,38 @@
+import URL from "url";
+
+/**
+ * Transforms link to embedded
+ * @param src Address of video
+ * @returns Url suitable for embedding
+ */
+const getEmbeddedLink = (src: string): string => {
+  const url = URL.parse(src, true);
+  if (!url.hostname) return src;
+
+  if (url.hostname.indexOf("youtube.com") > -1)
+    return `//www.youtube.com/embed/${url.query.v}`;
+
+  if (!url.pathname) return src;
+
+  if (url.hostname === "youtu.be")
+    return `//www.youtube.com/embed/${url.pathname.split("/")[1]}`;
+
+  if (url.hostname.indexOf("dailymotion.com") > -1)
+    return `//www.dailymotion.com/embed/video/${
+      url.pathname.split("/")[2].split("_")[0]
+    }`;
+
+  if (url.hostname.indexOf("dai.ly") > -1)
+    return `//www.dailymotion.com/embed/video/${url.pathname.split("/")[1]}`;
+
+  if (url.hostname.indexOf("vimeo.com") > -1) {
+    const match = /^(?:\/video|\/channels\/[\w-]+|\/groups\/[\w-]+\/videos)?\/(\d+)/.exec(
+      url.pathname
+    );
+    if (match) return `//player.vimeo.com/video/${match[1]}`;
+  }
+
+  return src;
+};
+
+export default getEmbeddedLink;
